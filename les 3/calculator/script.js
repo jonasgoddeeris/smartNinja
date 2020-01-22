9c9a507260c8f4a3b88e039fca65aaac343bc3b1
Jonas G <goddeerisjonas@me.com>
2020-01-22 09:12:44 +0100

    calculator

---
+++ les 3/calculator/script.js
@@ -0,0 +1,39 @@
+
+
+// // SUBMIT LISTENER 
+// let submitButton = document.getElementById("submit");
+// submitButton.addEventListener("click", calculate);
+
+
+// Click number LISTENER 
+let clickop7 = document.getElementById("7");
+clickop7.addEventListener('click', clickNumber);
+
+
+
+
+
+
+
+
+// function operation() {
+// 	let lights = document.getElementsByClassName("calbtn");
+// 	for (let i = 0; i < calbtn.length; i++) {
+//     let operator = calbtn[i].value;
+//     // console.log("makehitecounter" + i)
+// }
+
+
+// function calculate() {
+//     console.log("HELLO");
+// }
+
+// function clickop7() {
+//     console.log("HELLO7");
+// }
+
+
+
+function clickNumber() {
+	console.log('iets');
+};
